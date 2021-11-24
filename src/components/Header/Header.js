094c447b709dc3596f5da0c46c663bd958148ecb
Anton Yurchenko <webbymasta@gmail.com>
2021-11-24 20:06:26 +0200

    Sole&ankle workshop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,66 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
 
 const Header = () => {
-  // Our site features two visual headers, but they should be
-  // grouped semantically as a single header.
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Logo />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-      </MainHeader>
-    </header>
-  );
+    // Our site features two visual headers, but they should be
+    // grouped semantically as a single header.
+    return (
+        <header>
+            <SuperHeader />
+            <MainHeader>
+                <PositionedLogo />
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+            </MainHeader>
+        </header>
+    );
 };
 
-const MainHeader = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+const PositionedLogo = styled(Logo)`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto 0;
+    height: 28px;
+    line-height: 28px;
 `;
 
-const Nav = styled.nav``;
+const MainHeader = styled.div`
+    padding: 0 32px;
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    height: 74px;
+    display: flex;
+    align-items: center;
+    position: relative;
+`;
+
+const Nav = styled.nav`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    gap: 48px;
+`;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
 `;
 
 export default Header;
--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,86 +1,109 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { COLORS, WEIGHTS } from "../../constants";
+import { formatPrice, pluralize, isNewShoe } from "../../utils";
+import Spacer from "../Spacer";
 
-const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
-}) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
+const ShoeCard = ({ slug, name, imageSrc, price, salePrice, releaseDate, numOfColors }) => {
+    // There are 3 variants possible, based on the props:
+    //   - new-release
+    //   - on-sale
+    //   - default
+    //
+    // Any shoe released in the last month will be considered
+    // `new-release`. Any shoe with a `salePrice` will be
+    // on-sale. In theory, it is possible for a shoe to be
+    // both on-sale and new-release, but in this case, `on-sale`
+    // will triumph and be the variant used.
+    // prettier-ignore
+    const variant = typeof salePrice === 'number'
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
       : 'default'
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-        </Row>
-      </Wrapper>
-    </Link>
-  );
+    return (
+        <Link href={`/shoe/${slug}`}>
+            <Wrapper>
+                <ImageWrapper>
+                    <Image alt="" src={imageSrc} />
+                </ImageWrapper>
+                <Spacer size={12} />
+                <Row>
+                    <Name>{name}</Name>
+                    <Price variant={variant}>{formatPrice(price)}</Price>
+                </Row>
+                <Row>
+                    <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+                    {variant === "on-sale" && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
+                </Row>
+            </Wrapper>
+            {variant === "on-sale" && <Label variant={variant}>Sale</Label>}
+            {variant === "new-release" && <Label variant={variant}>Just released!</Label>}
+        </Link>
+    );
 };
 
-const Link = styled.a`
-  text-decoration: none;
-  color: inherit;
+const Label = styled.span`
+    position: absolute;
+    top: 12px;
+    right: -4px;
+    color: ${COLORS.white};
+    border-radius: 2px;
+    font-size: 14px;
+    font-weight: 700;
+    height: 32px;
+    line-height: 32px;
+    padding: 0 7px;
+    background: ${(props) => (props.variant === "on-sale" ? COLORS.primary : COLORS.secondary)};
 `;
 
-const Wrapper = styled.article``;
+const Link = styled.a`
+    text-decoration: none;
+    color: inherit;
+    flex: 1 1 270px;
+    position: relative;
+`;
+
+const Wrapper = styled.article`
+    display: flex;
+    flex-direction: column;
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
+    position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+    width: 100%;
+    border-radius: 16px 16px 4px 4px;
+`;
 
 const Row = styled.div`
-  font-size: 1rem;
+    font-size: 1rem;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const Name = styled.h3`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+    font-weight: 500;
+    font-size: {16/16}rem;
+    text-decoration: ${(props) => (props.variant === "on-sale" ? "line-through" : "none")};
+`;
 
 const ColorInfo = styled.p`
-  color: ${COLORS.gray[700]};
+    color: ${COLORS.gray[700]};
 `;
 
 const SalePrice = styled.span`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.primary};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.primary};
 `;
 
 export default ShoeCard;
